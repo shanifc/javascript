d698eea0a6828be4a1dbb17678a9af9bb64937a0
shanifc <mhdshanif43@gmail.com>
2025-09-13 12:01:15 +0530

    12/9/2025

--- rest-parameter/restWioth-named-parameters.js
+++ rest-parameter/restWioth-named-parameters.js
@@ -1,7 +1,7 @@
 // using rest parameters with named parameters
 // function to greet a person with any number of titles
 
-function greet(firstname, lastname,...titles) {
+function greet(firstname, lastname, ...titles) {
     console.log(`name: ${firstname} ${lastname}`);
     console.log(`titles: ${titles.join(", ")}`);
 }
